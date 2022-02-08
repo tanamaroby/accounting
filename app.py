b4c86ffc2fda2c31644eea9dba449b4cb7564e91
Roby Tanama <robytanma@gmail.com>
2022-02-08 23:29:31 +0800

    Change category to be more dynamic

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ data = journal()
 previous_time = None
 
 for row in df_array:
-    is_debit = math.isnan(row[3])
-    value = row[2] if is_debit else row[3]
+    is_debit = math.isnan(row[4])
+    value = row[3] if is_debit else row[4]
     time = previous_time if pd.isnull(row[0]) else row[0]
-    data.store_journal(row[1], is_debit, value, time)
+    data.store_journal(row[1], row[2], is_debit, value, time)
     previous_time = time
 
 data.print_journal()
--- model/journal.py
+++ model/journal.py
@@ -1,5 +1,4 @@
 # This class contains all journal entries categorized by accounts
-import database.account_types as at
 from model.entry import entry
 from model.account import account
 from model.category import category
@@ -8,16 +7,14 @@ class journal:
 
     def __init__(self):
         self.journal = {}
-        self.journal[at.get_assets()] = category(at.get_assets())
-        self.journal[at.get_liabilities()] = category(at.get_liabilities())
-        self.journal[at.get_equity()] = category(at.get_equity())
 
-    def store_journal(self, name, is_debit, value, time):
+    def store_journal(self, name, cat, is_debit, value, time):
         new_entry = entry(name, is_debit, value, time)
 
         # Storing in specific account types
-        account_type = at.get_account_type(name.lower())
-        self.journal[account_type].add_entry(new_entry)
+        if cat not in self.journal:
+            self.journal[cat] = category(cat)
+        self.journal[cat].add_entry(new_entry)
 
     def print_journal(self):
         for category in self.journal.keys():
